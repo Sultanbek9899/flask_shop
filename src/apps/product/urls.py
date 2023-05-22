d9ed3616339758f3697d9b886b6cac7c48a997d0
Sultan <sultanbek9899@gmail.com>
2023-05-22 20:09:03 +0600

    add models and serializer with modelviewsets

--- src/apps/product/urls.py
+++ src/apps/product/urls.py
@@ -1,4 +1,20 @@
 from django.urls import path
 
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = []
+from src.apps.product import views
+
+
+router = DefaultRouter()
+
+router.register("product", views.ProductViewSet, basename="product")
+router.register('category', views.CategoryViewSet, basename="category")
+
+
+urlpatterns = [
+   
+]
+
+
+
+urlpatterns += router.urls  
